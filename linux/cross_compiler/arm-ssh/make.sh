4bbc8bd677fc42c6f5b2e756e0454d205f47536a
wax100628 <wax628@gmail.com>
2017-03-01 23:14:31 +0800

    update download url

--- linux/cross_compiler/arm-ssh/make.sh
+++ linux/cross_compiler/arm-ssh/make.sh
@@ -22,7 +22,7 @@ mkdir -p $source_dir $compressed_dir $install_dir
 # 2. 下载源代码
 wget https://openbsd.hk/pub/OpenBSD/OpenSSH/portable/openssh-7.1p1.tar.gz -P $compressed_dir
 wget https://www.openssl.org/source/openssl-1.0.2f.tar.gz -P $compressed_dir
-wget http://www.zlib.net/fossils/zlib-1.2.3.tar.gz -P $compressed_dir
+wget http://www.zlib.net/fossils/zlib-1.2.8.tar.gz -P $compressed_dir
 
 # 3. 解压
 cd $compressed_dir
